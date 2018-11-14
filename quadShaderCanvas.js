3f3aabeb12409a7701b63f36e474a8c6c3c36ded
Weston C. Beecroft <westoncb@gmail.com>
2018-11-14 13:06:53 -0700

    Have QuadShaderCanvas take custom uniforms

--- quadShaderCanvas.js
+++ quadShaderCanvas.js
@@ -1,17 +1,21 @@
 const THREE = require('three');
 const Stats = require('stats-js');
+const AppState = require('./appState.js');
 const dat = require('dat.GUI');
 
 class QuadShaderCanvas {
-	constructor(containerElementId, fragmentShader) {
+	constructor(containerElementId, fragmentShader, customUniforms) {
 		this.containerElementId = containerElementId;
 		const containerElement = document.getElementById(this.containerElementId);
 
 		this.width = containerElement.offsetWidth;
 		this.height = containerElement.offsetHeight;
+		this.customUniforms = customUniforms || {};
 
 		this.initThreeJS();
 		this.initScene(fragmentShader);
+
+		this.updateCanvasSize();
 	}
 
 	initThreeJS() {
@@ -31,19 +35,22 @@ class QuadShaderCanvas {
 	    this.camera = new THREE.OrthographicCamera( this.width / - 2, this.width / 2, this.height / 2, this.height / - 2, 1, 2 );
 	    this.scene.add(this.camera);
 
-	    window.addEventListener( 'resize', this.onWindowResize.bind(this), false );
+	    window.addEventListener( 'resize', this.updateCanvasSize.bind(this), false );
 
-	    this.datgui = new dat.GUI( { width: 350 } );
+	    // this.datgui = new dat.GUI( { width: 350 } );
 	}
 
 	initScene(fragmentShader) {
+		const uniforms = {time: { value: 1.0 },
+					      resolution: { value: new THREE.Vector2(this.width, this.height)}};
+		Object.keys(this.customUniforms).forEach(uniformKey => {
+			uniforms[uniformKey] = this.customUniforms[uniformKey];
+		});
+
 		const geometry = new THREE.PlaneBufferGeometry(this.width, this.height);
 		const material = new THREE.RawShaderMaterial( {
 
-		    uniforms: {
-		        time: { value: 1.0 },
-		        resolution: { value: new THREE.Vector2(this.width, this.height) }
-		    },
+		    uniforms,
 		    vertexShader: this.getVertexShader(),
 		    fragmentShader,
 
@@ -73,12 +80,18 @@ class QuadShaderCanvas {
 	    this.stats.end();
 	}
 
-	onWindowResize( event ) {
+	updateCanvasSize() {
 	    const containerElement = document.getElementById(this.containerElementId);
 	    this.width = containerElement.offsetWidth;
 	    this.height = containerElement.offsetHeight;
 	    this.renderer.setSize( this.width, this.height );
 	    this.camera.updateProjectionMatrix();
+
+	    AppState.canvasWidth = this.width * window.devicePixelRatio;
+	    AppState.canvasHeight = this.height * window.devicePixelRatio;
+
+	    this.uniforms.resolution.value.x = AppState.canvasWidth;
+	    this.uniforms.resolution.value.y = AppState.canvasHeight;
 	}
 }
 
