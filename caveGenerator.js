351a7f31e33853856e84a9a1fb0737db7d0f37b3
Weston C. Beecroft <westoncb@gmail.com>
2018-11-15 13:45:43 -0700

    Move 1D noise stuff to Util
    And the step functions

--- caveGenerator.js
+++ caveGenerator.js
@@ -3,61 +3,9 @@ const AppState = require('./appState.js');
 
 const THREE = require('three');
 
-var Simple1DNoise = function() {
-    var MAX_VERTICES = 256;
-    var MAX_VERTICES_MASK = MAX_VERTICES -1;
-    var amplitude = 1;
-    var scale = 1;
-
-    var r = [];
-
-    for ( var i = 0; i < MAX_VERTICES; ++i ) {
-        r.push(Math.random());
-    }
-
-    var getVal = function( x ){
-        var scaledX = x * scale;
-        var xFloor = Math.floor(scaledX);
-        var t = scaledX - xFloor;
-        var tRemapSmoothstep = t * t * ( 3 - 2 * t );
-
-        /// Modulo using &
-        var xMin = xFloor & MAX_VERTICES_MASK;
-        var xMax = ( xMin + 1 ) & MAX_VERTICES_MASK;
-
-        var y = lerp( r[ xMin ], r[ xMax ], tRemapSmoothstep );
-
-        return y * amplitude;
-    };
-
-    /**
-    * Linear interpolation function.
-    * @param a The lower integer value
-    * @param b The upper integer value
-    * @param t The value between the two
-    * @returns {number}
-    */
-    var lerp = function(a, b, t ) {
-        return a * ( 1 - t ) + b * t;
-    };
-
-    // return the API
-    return {
-        getVal: getVal,
-        setAmplitude: function(newAmplitude) {
-            amplitude = newAmplitude;
-        },
-        setScale: function(newScale) {
-            scale = newScale;
-        }
-    };
-};
-
 class CaveGenerator {
 	constructor() {
 		this.junctures = [new THREE.Vector2(-100, 5), new THREE.Vector2(100, 5)]; //(x,y) points each time path slope changes
-
-		this.generator = new Simple1DNoise();
 	}
 
 	getTopSurfaceY(x) {
@@ -68,7 +16,7 @@ class CaveGenerator {
 	}
 
 	noise(x) {
-		return this.generator.getVal(x / 3) ** 2 * 4;
+		return Util.noise1d(x / 3) ** 2 * 4;
 	}
 
 	getBottomSurfaceY(x) {
--- util.js
+++ util.js
@@ -2,6 +2,59 @@ const THREE = require('three');
 
 const METERS_TO_PIXELS = 50;
 
+// https://www.michaelbromley.co.uk/blog/simple-1d-noise-in-javascript/
+var Simple1DNoise = function() {
+    var MAX_VERTICES = 256;
+    var MAX_VERTICES_MASK = MAX_VERTICES -1;
+    var amplitude = 1;
+    var scale = 1;
+
+    var r = [];
+
+    for ( var i = 0; i < MAX_VERTICES; ++i ) {
+        r.push(Math.random());
+    }
+
+    var getVal = function( x ){
+        var scaledX = x * scale;
+        var xFloor = Math.floor(scaledX);
+        var t = scaledX - xFloor;
+        var tRemapSmoothstep = t * t * ( 3 - 2 * t );
+
+        /// Modulo using &
+        var xMin = xFloor & MAX_VERTICES_MASK;
+        var xMax = ( xMin + 1 ) & MAX_VERTICES_MASK;
+
+        var y = lerp( r[ xMin ], r[ xMax ], tRemapSmoothstep );
+
+        return y * amplitude;
+    };
+
+    /**
+    * Linear interpolation function.
+    * @param a The lower integer value
+    * @param b The upper integer value
+    * @param t The value between the two
+    * @returns {number}
+    */
+    var lerp = function(a, b, t ) {
+        return a * ( 1 - t ) + b * t;
+    };
+
+    // return the API
+    return {
+        getVal: getVal,
+        setAmplitude: function(newAmplitude) {
+            amplitude = newAmplitude;
+        },
+        setScale: function(newScale) {
+            scale = newScale;
+        }
+    };
+};
+
+const noiseGenerator1d = new Simple1DNoise();
+
 class Util {
   // See https://hansmuller-webkit.blogspot.com/2013/02/where-is-mouse.html
   static canvasMousePos(event, canvas) {
@@ -51,6 +104,22 @@ class Util {
   static toMetersV(vec) {
     return vec.clone().multiplyScalar(1 / METERS_TO_PIXELS);
   }
+
+  static noise1d(x) {
+    return noiseGenerator1d.getVal(x);
+  }
+
+  static step(min, max, value) {
+    if (value <= min)
+      return 0;
+    if (value >= max)
+      return 1;
+  }
+
+  static smoothstep (min, max, value) {
+    const x = Math.max(0, Math.min(1, (value-min)/(max-min)));
+    return x*x*(3 - 2*x);
+  };
 }
 
 module.exports = Util;
